b873198a26453e5ffdad80359f364af5215b9e65
uyoung-jeong <farandole09@unist.ac.kr>
2022-12-09 12:44:03 +0900

    no weight saving

--- lib/models/mlp.py
+++ lib/models/mlp.py
@@ -23,67 +23,21 @@ class MLP():
         self.early_stopping = cfg.MODEL.MLP.EARLY_STOPPING
         self.beta1 = cfg.MODEL.MLP.BETA1
         self.beta2 = cfg.MODEL.MLP.BETA2
-        
-        
+
+
         self_attr = list(self.__dict__.keys())
         func_attr = list(inspect.signature(MLPRegressor).parameters)
         alter_dict = dict()
-        
+
         for attr in self_attr:
             if attr in func_attr:
                 alter_dict[attr] = getattr(self, attr)
-                
+
         self.model = MLPRegressor(**alter_dict)
-        
-    
-    def fit(self,x,y, save=True):
+
+    def fit(self,x,y):
         result = self.model.fit(x,y)
-        if save:
-            self.save_weight()
         return result
-    
+
     def predict(self,x):
         return self.model.predict(x)
-    
-    def save_weight(self):
-        os.makedirs('lib/weights', exist_ok=True)
-        if self.cfg.MODEL.MLP.SAVE_NAME:
-            split_name = self.cfg.MODEL.MLP.SAVE_NAME.split('.')[0]
-            check_name = split_name + '_5.npy'
-
-            if os.path.isfile(f'lib/weights/{check_name}'):
-                del_old_files = glob.glob(f'lib/weights/{split_name}*', recursive=True)
-                print('del_old_files : ', del_old_files)
-                for old_file in del_old_files:
-                    try:
-                        os.remove(old_file)
-                    except:
-                        pass
-
-            prior_list = sorted(glob.glob1('lib/weights/', f'{split_name}*'))
-            # print('prior_list :   ', prior_list)
-            if len(prior_list) == 0:
-                save_idx = 1
-            else:
-                save_idx = int(prior_list[-1].split('_')[2][0])+1
-            save_name = split_name+f'_{save_idx}.npy'
-
-            save_dict = {'coefs':self.model.coefs_, 'intercepts':self.model.intercepts_}
-            with open(f'lib/weights/{save_name}', 'wb') as f:
-                np.save(f, save_dict, allow_pickle=True)
-                print(save_name, 'saved')
-
-        else:
-            print(self.cfg.MODEL.MLP.SAVE_NAME)
-
-    def load_weight(self, idx):
-        file_name = self.cfg.MODEL.MLP.SAVE_NAME.split('.')[0]+f"_{idx+1}.npy"
-        with open(f'lib/weights/{file_name}', 'rb') as f:
-            weights = np.load(f, allow_pickle=True)
-        weights = weights.item()
-        num_coefs = len(weights['coefs'])
-
-        for idx in range(num_coefs):
-            self.model.coefs_[idx] = weights['coefs'][idx]
-            self.model.intercepts_[idx] = weights['intercepts'][idx]
-    
--- lib/models/mlp_svr.py
+++ lib/models/mlp_svr.py
@@ -11,28 +11,11 @@ class MLP_SVR():
         self.svr = SVR(cfg)
 
     def fit(self, x, y):
-        self.mlp.model.max_iter = 2
-        self.mlp.fit(x,y, save=False)
-        self.load_weight()
-        
-        # getting mlp's logit
-        train_logit = self.get_logit(x)
-        train_logit_target = y
-
-        self.svr = self.svr.fit(train_logit, train_logit_target)
+        self.mlp.fit(x,y)
+        mlp_preds = self.mlp.predict(x).reshape(-1,1)
+        self.svr.fit(mlp_preds, y)
         return self
-    
+
     def predict(self, x):
-        test_logit = self.get_logit(x)
-        scaler = StandardScaler()
-        scaler.fit(test_logit)
-        scaled_logit = scaler.transform(test_logit)
-        # print(test_logit[:3])
-        return self.svr.predict(scaled_logit)
-
-    def get_logit(self, x):
-        logit = self.mlp.predict(x).reshape(-1,1)
-        return logit
-
-    def load_weight(self):
-        self.mlp.load_weight(self.idx)
+        mlp_preds = self.mlp.predict(x).reshape(-1,1)
+        return self.svr.predict(mlp_preds)
