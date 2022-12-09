091a637d5a1f5b57c66a447a15a7edbe50b01e91
paul159 <50907810+paul159@users.noreply.github.com>
2022-12-09 10:47:40 +0900

    Update mlp.py

--- lib/models/mlp.py
+++ lib/models/mlp.py
@@ -1,9 +1,13 @@
 import inspect
+import numpy as np
+import os
+import glob
 from sklearn.neural_network import MLPRegressor
 
 
 class MLP():
     def __init__(self, cfg):
+        self.cfg = cfg
         self.hidden_layer_sizes = cfg.MODEL.MLP.HIDDEN_LAYER_SIZES
         self.activation = cfg.MODEL.MLP.ACTIVATION
         self.solver = cfg.MODEL.MLP.SOLVER
@@ -33,8 +37,52 @@ class MLP():
         
     
     def fit(self,x,y):
-        return self.model.fit(x,y)
-
+        result = self.model.fit(x,y)
+        if save:
+            self.save_weight()
+        return result
+    
     def predict(self,x):
         return self.model.predict(x)
+    
+    def save_weight(self):
+        if os.path.isdir(f'lib/weights') and self.cfg.MODEL.MLP.SAVE_NAME:
+            split_name = self.cfg.MODEL.MLP.SAVE_NAME.split('.')[0]
+            check_name = split_name + '_5.npy'
+
+            if os.path.isfile(f'lib/weights/{check_name}'):
+                del_old_files = glob.glob(f'lib/weights/{split_name}*', recursive=True)
+                print('del_old_files : ', del_old_files)
+                for old_file in del_old_files:
+                    try:
+                        os.remove(old_file)
+                    except:
+                        pass
+
+            prior_list = sorted(glob.glob1('lib/weights/', f'{split_name}*'))
+            # print('prior_list :   ', prior_list)
+            if len(prior_list) == 0:
+                save_idx = 1
+            else:
+                save_idx = int(prior_list[-1].split('_')[2][0])+1
+            save_name = split_name+f'_{save_idx}.npy'
+
+            save_dict = {'coefs':self.model.coefs_, 'intercepts':self.model.intercepts_}
+            with open(f'lib/weights/{save_name}', 'wb') as f:
+                np.save(f, save_dict, allow_pickle=True)
+                print(save_name, 'saved')
+
+        else:
+            print(self.cfg.MODEL.MLP.SAVE_NAME)
+
+    def load_weight(self, idx):
+        file_name = self.cfg.MODEL.MLP.SAVE_NAME.split('.')[0]+f"_{idx+1}.npy"
+        with open(f'lib/weights/{file_name}', 'rb') as f:
+            weights = np.load(f, allow_pickle=True)
+        weights = weights.item()
+        num_coefs = len(weights['coefs'])
+
+        for idx in range(num_coefs):
+            self.model.coefs_[idx] = weights['coefs'][idx]
+            self.model.intercepts_[idx] = weights['intercepts'][idx]
     
